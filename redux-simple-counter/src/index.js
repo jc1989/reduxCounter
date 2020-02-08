16596ad863adc8286f2fc529b2cbf01e0d616204
Joshua Cannedy <jdcannedy@gmail.com>
2020-02-08 12:28:26 -0600

    added color change

--- redux-simple-counter/src/index.js
+++ redux-simple-counter/src/index.js
@@ -7,6 +7,7 @@ const { subscribe, dispatch, getState } = createStore(reducer);
 // Here we pass the Redux state to our render method (defined globally in render.js)
 subscribe(() => render(getState()))
 
+// Dispatch the "DECREMENT" action every time the -1 button is pressed
 const decrementButton = document.getElementById('decrement');
 decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
 
@@ -14,11 +15,20 @@ decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
 const incrementButton = document.getElementById('increment');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
 
+// Dispatch the "INCREMENT" action every time the +5 button is pressed
 const incrementFiveButton = document.getElementById('incrementFive');
 incrementFiveButton.addEventListener('click', e => dispatch({ type: "INCREMENTFIVE" }));
 
+// Dispatch the "DECREMENT" action every time the -5 button is pressed
 const decrementFiveButton = document.getElementById('decrementFive');
 decrementFiveButton.addEventListener('click', e => dispatch({ type: "DECREMENTFIVE" }));
 
+// Dispatch the "ZERO" action every time the Zero button is pressed
 const zeroButton = document.getElementById('zero');
 zeroButton.addEventListener('click', e => dispatch({ type: "ZERO" }));
+
+// Dispatch the "COLOR" action to change the color
+const colors = document.getElementById('colors');
+colors.addEventListener('change', e => {
+    dispatch({ type: "COLORS", color: e.target.value })
+});
--- redux-simple-counter/src/reducer.js
+++ redux-simple-counter/src/reducer.js
@@ -1,37 +1,44 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: "#ffffff"
 }
 
 const reducer = (state = initialState, action) => {
-    let { type } = action;
+    let { type, color } = action;
     if (type === "INCREMENT") {
         return {
-            value: state.value + 1
+            value: state.value + 1,
+            color: state.color
         }
     }
     else if (type === "DECREMENT") {
         return {
-            value: state.value - 1
+            value: state.value - 1,
+            color: state.color
         }
     }
     else if (type === "ZERO"){
         return {
-            value: 0
+            value: 0,
+            color: state.color
         }
     }
     else if (type === "INCREMENTFIVE"){
         return {
-            value: state.value + 5
+            value: state.value + 5,
+            color: state.color
         }
     }
     else if (type === "DECREMENTFIVE"){
         return {
-            value: state.value - 5
+            value: state.value - 5,
+            color: state.color
         }
     }
-    else if (type === "USERENTRY"){
+    else if (type === "COLORS"){
         return {
-            value: state.value
+            value: state.value,
+            color: color
         }
     }
     return state
